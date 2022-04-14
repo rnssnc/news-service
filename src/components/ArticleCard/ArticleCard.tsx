4b68c646e45bf7f728337af45617b1540e7edaf2
eminov.rnssnc <eminov.rnssnc@gmail.com>
2022-04-15 02:05:48 +0300

    feat: добавлен компонент карточки новости

---
+++ src/components/ArticleCard/ArticleCard.tsx
@@ -0,0 +1,78 @@
+import { cn } from "@bem-react/classname";
+import { Link } from "components/Link/Link";
+import React, { useCallback, useMemo, useState } from "react"
+
+import { IArticle } from "services/NewsService/NewsService";
+import { IClassNameProps } from "utils/core";
+import { getFaviconURL } from "utils/helpers/getFaviconURL";
+
+import './ArticleCard.scss';
+
+const newsCardCn = cn('ArticleCard');
+const cnArticleCardMedia = newsCardCn('Media');
+const cnArticleCardImage = newsCardCn('Image');
+const cnArticleCardTextContent = newsCardCn('TextContent');
+const cnArticleCardTitle = newsCardCn('Title');
+const cnArticleCardDescription = newsCardCn('Description');
+const cnArticleCardFooter = newsCardCn('Footer');
+const cnArticleCardSource = newsCardCn('Source');
+const cnArticleCardDomain = newsCardCn('Domain');
+const cnArticleCardFavicon = newsCardCn('Favicon');
+const cnArticleCardDate = newsCardCn('Date');
+
+export interface IArticleCardProps extends IClassNameProps {
+  article: IArticle;
+  isHeadline?: boolean;
+  cardRef?: ((node: HTMLDivElement) => () => void) | null;
+}
+
+export const ArticleCard: React.FC<IArticleCardProps> = ({
+  className,
+  article,
+  isHeadline,
+  cardRef,
+}) => {
+  const [imgLoadingError, setImgLoadingError] = useState(false);
+  const publishedDate = useMemo(() => new Date(article.publishedAt), [article.publishedAt]);
+
+  const isDateToday = useCallback((date: Date) => {
+    const otherDate = new Date(date);
+    const todayDate = new Date();
+
+    return (
+      otherDate.getDate() === todayDate.getDate() &&
+      otherDate.getMonth() === todayDate.getMonth() &&
+      otherDate.getFullYear() === todayDate.getFullYear()
+    );
+  }, []);
+
+  const footer = (
+    <div className={cnArticleCardFooter}>
+      <div className={cnArticleCardSource}>
+        <span className={cnArticleCardFavicon} style={{ backgroundImage: `url('${getFaviconURL(article.url)}')` }} />
+        <span className={cnArticleCardDomain}>
+          {article.source.name}
+        </span>
+      </div>
+      <div className={cnArticleCardDate}>{
+        isDateToday(publishedDate) ?
+          `Сегодня, ${publishedDate.getHours() < 10 ? `0${publishedDate.getHours()}` : publishedDate.getHours()}:${publishedDate.getMinutes() < 10 ? `0${publishedDate.getMinutes()}` : publishedDate.getMinutes()}` :
+          publishedDate.toLocaleDateString(undefined)
+      }</div>
+    </div>
+  );
+
+  return (
+    <Link className={newsCardCn({ Headline: Boolean(isHeadline) }, [className])} href={article.url}>
+        <div className={cnArticleCardTextContent} ref={cardRef}>
+          <h3 className={cnArticleCardTitle}>{article.title}</h3>      
+          <div className={cnArticleCardDescription}>{article.description}</div> 
+          { isHeadline && footer } 
+        </div>
+        <div className={cnArticleCardMedia}>
+          <img className={cnArticleCardImage} alt="Изображение новости" src={imgLoadingError ? `${process.env.PUBLIC_URL}/assets/svg/news.svg` : article.urlToImage || `${process.env.PUBLIC_URL}/assets/svg/news.svg`} onError={() => setImgLoadingError(true)} />
+        </div>
+        { !isHeadline && footer }
+    </Link>
+  )
+};
