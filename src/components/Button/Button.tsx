8fe50b31cf62bc13f061cea7614dd5e9e0b16a22
eminov.rnssnc <eminov.rnssnc@gmail.com>
2022-04-15 02:06:06 +0300

    feat: добавлен базовый компонент кнопки

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { cn } from "@bem-react/classname";
+ 
+import { IClassNameProps } from "utils/core";
+
+import './Button.scss';
+import { Icon as IconComponent, IIconProps } from "components/Icon/Icon";
+
+const buttonCn = cn('Button');
+
+export interface IButtonProps extends IClassNameProps, Partial<IIconProps> {
+  onClick?(): void;
+}
+
+export const Button: React.FC<IButtonProps> = ({
+  className,
+  Icon,
+  size,
+  children: text,
+  onClick,
+}) => {
+  return (
+    <button className={buttonCn({ 
+      Icon: Boolean(Icon && !text), 
+      WithIcon: Boolean(Icon && text), 
+    }, [className])}
+      onClick={onClick}
+    >
+      { Icon && (
+        <IconComponent 
+          Icon={Icon}
+          size={size || 'm'}
+        />
+      )}
+      {text}
+    </button>
+  )
+};
